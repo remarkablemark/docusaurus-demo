4aabead85d4dea5a2b343be9a6b262658996b574
Mark <remarkablemark@users.noreply.github.com>
2022-12-01 00:36:20 -0500

    build: add env vars to docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: process.env.URL || 'https://your-docusaurus-test-site.com',
   baseUrl: process.env.BASE_URL || '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: process.env.ORGANIZATION_NAME || 'facebook', // Usually your GitHub org/user name.
+  projectName: process.env.PROJECT_NAME || 'docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
